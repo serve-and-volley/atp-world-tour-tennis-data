2df3e5904eb8e97e4ad5a49b661c48f0ebf48dd0
Kevin <kevin.j.lin@gmail.com>
2017-10-28 11:37:58 -0700

    SQL files
    Deprecated current files and added new one.

---
+++ sql/postgres_tables.sql
@@ -0,0 +1,138 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * *
+*                                                *
+*   The following is for a PostgreSQL database   *   
+*                                                *
+* * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+/* TOURNAMENTS */
+CREATE TABLE tournaments (
+    id                              integer,
+    tourney_year                    integer,
+    tourney_order                   integer,
+    tourney_name                    varchar(60),
+    tourney_id                      integer,
+    tourney_slug                    varchar(30),
+    tourney_location                varchar(40),
+    tourney_dates                   varchar(10),
+    tourney_month                   integer,
+    tourney_day                     integer,
+    tourney_singles_draw            integer,
+    tourney_doubles_draw            integer,
+    tourney_conditions              varchar(10),
+    tourney_surface                 varchar(10),
+    tourney_fin_commit              varchar(15),
+    tourney_url_suffix              varchar(200),
+    singles_winner_name             varchar(40),
+    singles_winner_url              varchar(200),
+    singles_winner_player_slug      varchar(40),
+    singles_winner_player_id        varchar(10),
+    doubles_winner_1_name           varchar(40),
+    doubles_winner_1_url            varchar(200),
+    doubles_winner_1_player_slug    varchar(40),
+    doubles_winner_1_player_id      varchar(10),
+    doubles_winner_2_name           varchar(40),
+    doubles_winner_2_url            varchar(200),
+    doubles_winner_2_player_slug    varchar(40),
+    doubles_winner_2_player_id      varchar(10),
+    tourney_year_id                 varchar(15)
+);
+
+-- Ingesting the 'tournaments' indexed CSV file
+\copy tournaments FROM '~/Desktop/test_atp/_output/1_tournaments/tournaments_indexed_1877-2016.csv' DELIMITER ',' CSV
+
+
+/* MATCH SCORES */
+CREATE TABLE match_scores (
+    id                      integer,
+    tourney_year_id         varchar(15),
+    tourney_order           integer,
+    tourney_slug            varchar(30),
+    tourney_url_suffix      varchar(200),
+    tourney_round_name      varchar(20),
+    round_order             integer,
+    match_order             integer,
+    winner_name             varchar(40),
+    winner_player_id        varchar(10),
+    winner_slug             varchar(40),
+    loser_name              varchar(40),
+    loser_player_id         varchar(10),
+    loser_slug              varchar(40),
+    winner_seed             varchar(5),
+    loser_seed              varchar(5),
+    match_score_tiebreaks   varchar(40),
+    winner_sets_won         integer,
+    loser_sets_won          integer,
+    winner_games_won        integer,
+    loser_games_won         integer,
+    winner_tiebreaks_won    integer,
+    loser_tiebreaks_won     integer,
+    match_id                varchar(40),
+    match_stats_url_suffix  varchar(200)
+);
+
+-- Ingesting the 'match_scores' indexed CSV file
+\copy match_scores FROM '~/Desktop/test_atp/_output/2_match_scores/match_scores_indexed_1877-2016.csv' DELIMITER ',' CSV
+
+/* MATCH STATS */
+CREATE TABLE match_stats (
+    id                                  integer,
+    tourney_order                       integer,
+    match_id                            varchar(40),
+    match_stats_url_suffix              varchar(200),
+    match_time                          varchar(10),
+    match_duration                      integer,
+    winner_aces                         integer,
+    winner_double_faults                integer,
+    winner_first_serves_in              integer,
+    winner_first_serves_total           integer,
+    winner_first_serve_points_won       integer,
+    winner_first_serve_points_total     integer,
+    winner_second_serve_points_won      integer,
+    winner_second_serve_points_total    integer,
+    winner_break_points_saved           integer,
+    winner_break_points_serve_total     integer,
+    winner_service_points_won           integer,
+    winner_service_points_total         integer,
+    winner_first_serve_return_won       integer,
+    winner_first_serve_return_total     integer,
+    winner_second_serve_return_won      integer,
+    winner_second_serve_return_total    integer,
+    winner_break_points_converted       integer,
+    winner_break_points_return_total    integer,
+    winner_service_games_played         integer,
+    winner_return_games_played          integer,
+    winner_return_points_won            integer,
+    winner_return_points_total          integer,
+    winner_total_points_won             integer,
+    winner_total_points_total           integer,
+    loser_aces                          integer,
+    loser_double_faults                 integer,
+    loser_first_serves_in               integer,
+    loser_first_serves_total            integer,
+    loser_first_serve_points_won        integer,
+    loser_first_serve_points_total      integer,
+    loser_second_serve_points_won       integer,
+    loser_second_serve_points_total     integer,
+    loser_break_points_saved            integer,
+    loser_break_points_serve_total      integer,
+    loser_service_points_won            integer,
+    loser_service_points_total          integer,
+    loser_first_serve_return_won        integer,
+    loser_first_serve_return_total      integer,
+    loser_second_serve_return_won       integer,
+    loser_second_serve_return_total     integer,
+    loser_break_points_converted        integer,
+    loser_break_points_return_total     integer,
+    loser_service_games_played          integer,
+    loser_return_games_played           integer,
+    loser_return_points_won             integer,
+    loser_return_points_total           integer,
+    loser_total_points_won              integer,
+    loser_total_points_total            integer
+);
+
+-- Ingesting the 'match_stats' indexed CSV file
+\copy match_stats FROM '~/Desktop/test_atp/_output/3_match_stats/match_stats_indexed_1991-2016.csv' DELIMITER ',' CSV
+
+
